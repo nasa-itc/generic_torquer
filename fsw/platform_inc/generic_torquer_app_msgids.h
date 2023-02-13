28ce1c4ccc911602d7b3a99a1a49442b6aa1cfa4
Lucas, John P <John.P.Lucas@nasa.gov>
2023-02-13 16:37:37 -0500

    [nasa/nos3#105] Updated MSGIDs and changes simulator executable name to use dashes;

--- fsw/platform_inc/generic_torquer_app_msgids.h
+++ fsw/platform_inc/generic_torquer_app_msgids.h
@@ -12,11 +12,10 @@
 #ifndef _generic_torquer_app_msgids_h_
 #define _generic_torquer_app_msgids_h_
 
-#define GENERIC_TORQUER_APP_CMD_MID           0x18FA
-#define GENERIC_TORQUER_APP_SEND_HK_MID       0x18FB
-#define GENERIC_TORQUER_APP_HK_TLM_MID        0x08FA
-#define GENERIC_TORQUER_APP_SEND_DEV_HK_MID   0x08FB
-#define GENERIC_TORQUER_APP_SEND_DEV_DATA_MID 0x08FC
+#define GENERIC_TORQUER_APP_CMD_MID           0x193A
+#define GENERIC_TORQUER_APP_SEND_HK_MID       0x193B
+
+#define GENERIC_TORQUER_APP_HK_TLM_MID        0x093A
 
 #endif /* _generic_torquer_app_msgids_h_ */
 
