3effc253d0009f2e731c116f13e0c82f2236f695
dacarter22 <98733749+dacarter22@users.noreply.github.com>
2025-03-24 19:05:26 +0000

    style: auto-format via clang-format

--- fsw/shared/generic_torquer_device.h
+++ fsw/shared/generic_torquer_device.h
@@ -12,30 +12,30 @@
 ** Required header files.
 */
 #include "device_cfg.h"
-#include "hwlib.h"
 #include "generic_torquer_platform_cfg.h"
-
+#include "hwlib.h"
 
 /*
 ** Type definitions
 */
-#define GENERIC_TORQUER_DEVICE_DISABLED       0
-#define GENERIC_TORQUER_DEVICE_ENABLED        1
+#define GENERIC_TORQUER_DEVICE_DISABLED 0
+#define GENERIC_TORQUER_DEVICE_ENABLED 1
 
 /*
 ** GENERIC_TORQUER device telemetry definition
 */
-typedef struct 
-{
-    uint8_t Direction;
-    uint8_t PercentOn;
+typedef struct {
+  uint8_t Direction;
+  uint8_t PercentOn;
 
 } __attribute__((packed)) GENERIC_TORQUER_Device_tlm_t;
-#define GENERIC_TORQUER_DEVICE_TLM_LNGTH sizeof ( GENERIC_TORQUER_Device_tlm_t )
+#define GENERIC_TORQUER_DEVICE_TLM_LNGTH sizeof(GENERIC_TORQUER_Device_tlm_t)
 
 /*
 ** Prototypes
 */
-int32_t GENERIC_TORQUER_Config(GENERIC_TORQUER_Device_tlm_t* trqHk, trq_info_t* trqDevice, uint8_t percent, uint8_t dir);
+int32_t GENERIC_TORQUER_Config(GENERIC_TORQUER_Device_tlm_t *trqHk,
+                               trq_info_t *trqDevice, uint8_t percent,
+                               uint8_t dir);
 
 #endif /* _GENERIC_TORQUER_DEVICE_H_ */
