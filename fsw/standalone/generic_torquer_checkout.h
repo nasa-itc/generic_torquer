98d73b622cf5f8df9dbe7e618ecdca34b5e13809
rjbrown2 <91291114+rjbrown2@users.noreply.github.com>
2025-03-25 16:14:13 +0000

    style: auto-format via clang-format

--- fsw/standalone/generic_torquer_checkout.h
+++ fsw/standalone/generic_torquer_checkout.h
@@ -28,39 +28,39 @@
 #include "generic_torquer_device.h"
 
 #if TGTNAME == cpu1
-    #include "nos_link.h"
+#include "nos_link.h"
 #endif
 
 /*
 ** Standard Defines
 */
-#define PROMPT                  "generic_torquer> "
-#define MAX_INPUT_BUF         	512
-#define MAX_INPUT_TOKENS      	64
-#define MAX_INPUT_TOKEN_SIZE  	50
-#define TELEM_BUF_LEN           8
+#define PROMPT               "generic_torquer> "
+#define MAX_INPUT_BUF        512
+#define MAX_INPUT_TOKENS     64
+#define MAX_INPUT_TOKEN_SIZE 50
+#define TELEM_BUF_LEN        8
 
 /*
 ** Command Defines
 */
 #define CMD_UNKNOWN -1
-#define CMD_HELP     0
-#define CMD_EXIT     1
-#define CMD_NOOP     2
-#define CMD_HK       3
-#define CMD_TORQUE   4
+#define CMD_HELP    0
+#define CMD_EXIT    1
+#define CMD_NOOP    2
+#define CMD_HK      3
+#define CMD_TORQUE  4
 
 /*
 ** Prototypes
 */
 void print_help(void);
-int  get_command(const char* str);
+int  get_command(const char *str);
 int  main(int argc, char *argv[]);
 
 /*
 ** Generic Prototypes
 */
-int check_number_arguments(int actual, int expected);
-void to_lower(char* str);
+int  check_number_arguments(int actual, int expected);
+void to_lower(char *str);
 
 #endif /* _GENERIC_TORQUER_CHECKOUT_H_ */
