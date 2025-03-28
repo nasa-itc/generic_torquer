211863e22132de568542624abc49084123d3a4c4
dacarter22 <davidacarter22@gmail.com>
2025-03-28 10:59:36 -0400

    [nasa/nos3#587] New branch and cpt tests started saving progress

--- gsw/GENERIC_TORQUER/procedures/generic_torquer_test.rb
+++ gsw/GENERIC_TORQUER/procedures/generic_torquer_test.rb
@@ -2,25 +2,27 @@ require 'cosmos'
 require 'cosmos/script'
 require 'mission_lib.rb'
 
-class LPT < Cosmos::Test
+class TORQUER_LPT < Cosmos::Test
   def setup
-      enable_TO_and_verify()
+    
   end
 
   def test_lpt
+    start("tests/generic_torquer_lpt_test.rb")
   end
 
   def teardown
-      cmd("CFS_RADIO TO_PAUSE_OUTPUT")
+
   end
 end
 
-class CPT < Cosmos::Test
+class TORQUER_CPT < Cosmos::Test
   def setup
       
   end
 
   def test_cpt
+    start("tests/generic_torquer_cpt_test.rb")
   end
 
   def teardown
@@ -31,8 +33,8 @@ end
 class Generic_torquer_Test < Cosmos::TestSuite
   def initialize
       super()
-      add_test('CPT')
-      add_test('LPT')
+      add_test('TORQUER_CPT')
+      add_test('TORQUER_LPT')
   end
 
   def setup
--- gsw/GENERIC_TORQUER/procedures/tests/generic_torquer_cpt_test.rb
+++ gsw/GENERIC_TORQUER/procedures/tests/generic_torquer_cpt_test.rb
@@ -1,26 +1,170 @@
 require 'cosmos'
 require 'cosmos/script'
 require "cfs_lib.rb"
+#require 'math'
 
+##
+## NOOP
+##
+initial_command_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_NOOP_CC")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
 
-# initial_command_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_COUNT")
-# initial_error_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT")
-# initial_device_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT")
-# initial_device_error_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT")
-# cmd("SAMPLE SAMPLE_NOOP_CC")
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT == #{initial_device_count}", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+sleep(5)
 
-# sleep(5)
+##
+## Successful Disable
+##
+initial_command_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_DISABLE_CC")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ENABLED == 'DISABLED'", 30)
 
-# initial_command_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_COUNT")
-# initial_error_count = tlm("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT")
-# initial_device_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT")
-# initial_device_error_count = tlm("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT")
-# cmd("SAMPLE SAMPLE_RST_COUNTERS_CC")
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM CMD_ERR_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_COUNT == 0", 30)
-# wait_check("SAMPLE SAMPLE_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+sleep(5)
+
+##
+## Failed Disable (doubled)
+##
+initial_command_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_DISABLE_CC")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ENABLED == 'DISABLED'", 30)
+
+sleep(5)
+
+##
+## Device Config
+##
+initial_command_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+trq_num = ask "Enter Configuration Value (integer between 0 and 2):"
+direction = ask "Enter Configuration Value (integer between 0 and 1):"
+percent = ask "Enter Configuration Value (integer between 0 and 100):"
+cmd("GENERIC_TORQUER GENERIC_TORQUER_PERCENT_ON_CC with TRQNUM #{trq_num} DIRECTION #{direction} PERCENT_ON #{percent}")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## HK without Device
+##
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_REQ_HK")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data without Device
+##
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_REQ_DATA")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Successful Enable
+##
+initial_command_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_ENABLE_CC")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Failed Enable (doubled)
+##
+initial_command_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT")
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_ENABLE_CC")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT > #{initial_command_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT > #{initial_device_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ENABLED == 'ENABLED'", 30)
+
+sleep(5)
+
+##
+## Housekeeping w/ Device
+##
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+cmd("GENERIC_TORQUER GENERIC_TORQUER_REQ_HK")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+
+sleep(5)
+
+##
+## Data w/ Device
+##
+
+initial_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT")
+initial_device_error_count = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT")
+
+cmd("GENERIC_TORQUER GENERIC_TORQUER_REQ_DATA")
+
+truth_42_wn_0 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA W_0")
+truth_42_wn_1 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA W_1")
+truth_42_wn_3 = tlm("SIM_42_TRUTH SIM_42_TRUTH_DATA W_2")
+
+torquer_percent0 = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM_T TORQUER_PERCENT_ON_0")
+torquer_percent1 = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM_T TORQUER_PERCENT_ON_1")
+torquer_percent2 = tlm("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM_T TORQUER_PERCENT_ON_2")
+
+truth_42_wn_diff_0 = (torquer_percent0 - truth_42_wn_0).abs()
+truth_42_wn_diff_1  = (torquer_percent1 - truth_42_wn_1).abs()
+truth_42_wn_diff_2 = (torquer_percent2 - truth_42_wn_2).abs()
+
+diff_margin = 0.025
+
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == #{initial_error_count}", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == #{initial_device_error_count}", 30)
+if torquer_error == 0
+  wait_check_expression("truth_42_alpha_diff <= diff_margin # #{truth_42_alpha_diff} <= #{diff_margin}", 15)
+
+  wait_check_expression("truth_42_beta_diff <= diff_margin # #{truth_42_beta_diff} <= #{diff_margin}", 15)
+end
+
+sleep(5)
+
+##
+## Reset Counters
+##
+cmd("GENERIC_TORQUER GENERIC_TORQUER_DISABLE_CC") # disable to be able to properly check that device count gets reset
+cmd("GENERIC_TORQUER GENERIC_TORQUER_RST_COUNTERS_CC")
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_COUNT == 0", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM CMD_ERR_COUNT == 0", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_COUNT == 0", 30)
+wait_check("GENERIC_TORQUER GENERIC_TORQUER_HK_TLM DEVICE_ERR_COUNT == 0", 30)
+
+##
+## Reenable so test can be run multiple times
+##
+cmd("GENERIC_TORQUER GENERIC_TORQUER_ENABLE_CC")
