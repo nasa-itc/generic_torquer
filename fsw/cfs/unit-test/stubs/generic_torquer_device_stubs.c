6cfb4522da76f6b3776f509a231b96669fac8254
dacarter22 <davidacarter22@gmail.com>
2025-05-01 17:30:35 +0000

    [nasa/nos3#460] corrected stubs with ut_generator

--- fsw/cfs/unit-test/stubs/generic_torquer_device_stubs.c
+++ fsw/cfs/unit-test/stubs/generic_torquer_device_stubs.c
@@ -1,19 +1,36 @@
-#include "utgenstub.h"
-#include "generic_torquer_device.h"
+/*******************************************************************************
+** File: generic_torquer_device.h
+**
+** Purpose:
+**   This is the header file for the GENERIC_TORQUER device.
+**
+*******************************************************************************/
+#define _GENERIC_TORQUER_DEVICE_H_
 
+/**
+ * @file
+ *
+ * Auto-Generated stub implementations for functions defined in generic_torquer_device header
+ */
+
+#include "generic_torquer_device.h"
+#include "utgenstub.h"
+
+/*
+ * ----------------------------------------------------
+ * Generated stub function for GENERIC_TORQUER_Config()
+ * ----------------------------------------------------
+ */
 int32_t GENERIC_TORQUER_Config(GENERIC_TORQUER_Device_tlm_t *trqHk, trq_info_t *trqDevice, uint8_t percent, uint8_t dir)
 {
     UT_GenStub_SetupReturnBuffer(GENERIC_TORQUER_Config, int32_t);
+
     UT_GenStub_AddParam(GENERIC_TORQUER_Config, GENERIC_TORQUER_Device_tlm_t *, trqHk);
     UT_GenStub_AddParam(GENERIC_TORQUER_Config, trq_info_t *, trqDevice);
     UT_GenStub_AddParam(GENERIC_TORQUER_Config, uint8_t, percent);
     UT_GenStub_AddParam(GENERIC_TORQUER_Config, uint8_t, dir);
 
-    status = trq_command(trqDevice, percent, dir);
-    if (status == OS_SUCCESS)
-    {
-        trqHk->PercentOn = percent;
-        trqHk->Direction = dir;
-    }
-    return status;
+    UT_GenStub_Execute(GENERIC_TORQUER_Config, Basic, NULL);
+
+    return UT_GenStub_GetReturnValue(GENERIC_TORQUER_Config, int32_t);
 }
